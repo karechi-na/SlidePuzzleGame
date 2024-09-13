f13e78b0e30da7e3dd93d09c7d2964088d9a0e40
game\242j005 <ttno6018@gmail.com>
2024-09-13 15:26:20 +0900

    ﻿スタート画面からプレイ画面の切り替え、クリアとゲームオーバーからスタート画面の切り替えをできるようにしました。

---
+++ SlideNumberPuzzle/Assets/Scenes/ToGameSceneButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToGameSceneButton : MonoBehaviour
+{
+    
+    public void OnClicToGameSceneButton()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+}
